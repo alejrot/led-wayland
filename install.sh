4939dfe4252b3278c56c1f3184321cb733b76fcd
“Marcelo <marotmarcelo@gmail.com>
2024-11-15 17:41:58 -0300

    Install, uninstall and service updated

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
+echo "Installing LEDs control for Wayland"
+echo "It assumes Systemd's service is enabled"
+
+# stops tasks if they're already working
 sudo systemctl stop keyboard_leds
 
-sudo cp keyboard_leds.sh /usr/bin/keyboard_leds.sh
-sudo chmod +x /usr/bin/keyboard_leds.sh
 
+# periodic task - pushes LED's status (to ON by default)
+sudo cp keyboard_leds.sh /usr/local/bin/keyboard_leds.sh
+sudo chmod +x /usr/local/bin/keyboard_leds.sh
+# Systemd service - initializes routine after boot
 sudo cp keyboard_leds.service /etc/systemd/system/keyboard_leds.service
-sudo chmod 644  /etc/systemd/system/keyboard_leds.service
+# sudo chmod +x  /etc/systemd/system/keyboard_leds.service
 
+# trigger task - sends signal to LED's manager
+sudo cp switching.sh   /usr/local/bin/keyboard_switch.sh
+sudo chmod +x  /usr/local/bin/keyboard_switch.sh
+
+
+# enables auto-init 
 sudo systemctl enable keyboard_leds
 sudo systemctl daemon-reload
 sudo systemctl start keyboard_leds
 
+echo "Done!"
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
-
+# stops services and routines
 sudo systemctl stop keyboard_leds
+pid=`pgrep keyboard_leds`
+kill -SIGKILL  $pid
 
-sudo rm /usr/bin/keyboard_leds.sh
+
+# removing all routines and services
+sudo rm /usr/local/bin/keyboard_leds.sh
+sudo rm /usr/local/bin/keyboard_switch.sh
 sudo rm /etc/systemd/system/keyboard_leds.service
 
+# reloading services - forcing initialization
 sudo systemctl daemon-reload
